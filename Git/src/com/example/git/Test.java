327ae179f541070fedd2b448aa2bc068b9481905
hector.ayestaran@gmail.com <hector.ayestaran@gmail.com>
2022-04-05 20:57:13 +0200

    Dos atributos.

--- Git/src/com/example/git/Test.java
+++ Git/src/com/example/git/Test.java
@@ -2,7 +2,10 @@ package com.example.git;
 
 public class Test {
 	
+	// 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
+	
 	public int a;
+	private int b;
 
 	public Test() {
 		// TODO Auto-generated constructor stub
