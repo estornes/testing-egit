13505b83c6f168c738b907468f9a007fcd4a5f9f
hector.ayestaran@gmail.com <hector.ayestaran@gmail.com>
2022-04-07 18:18:22 +0200

    Atributo 4 cambiado.

--- Git/src/com/example/git/Test.java
+++ Git/src/com/example/git/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public int a = 2;
 	private int b = 3;
 	private int c = 4;
-	private int d = 7;
+	private int d = 8;
 
 	public Test() {
 		// TODO Auto-generated constructor stub
